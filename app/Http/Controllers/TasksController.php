46fd76da4aeb66bfd77b8a5c9ed5d013b9b0a6a8
shunichi.kito <shunichi.kito@rakuten.com>
2018-05-29 01:11:42 +0000

    additional status column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,8 +48,13 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
@@ -97,8 +102,13 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         //
+         $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status; 
         $task->save();
 
         return redirect('/');
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -9,6 +9,9 @@
 
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
+        
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::submit('更新') !!}
 
