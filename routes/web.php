13d3432c9779185425ac3fddad3ea59c7ed4547f
shunichi.kito <shunichi.kito@rakuten.com>
2018-06-12 11:59:17 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 // CRUD
 // Route::get('tasks/{id}', 'tasksController@show');
 // Route::post('tasks', 'tasksController@store');
